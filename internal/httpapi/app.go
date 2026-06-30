a88f7a0f52834547c7ea983cfda2116f60e5d0d8
hilbert <gstartwinkler@gmail.com>
2026-06-30 18:10:38 +0800

    65-改UI名称的chatgpt2api

--- internal/httpapi/app.go
+++ internal/httpapi/app.go
@@ -653,8 +653,8 @@ func (a *App) handleSettings(w http.ResponseWriter, r *http.Request) {
 
 func (a *App) handleAppMeta(w http.ResponseWriter, r *http.Request) {
 	util.WriteJSON(w, http.StatusOK, map[string]any{
-		"app_title":                   "chatgpt2api",
-		"project_name":                "chatgpt2api",
+		"app_title":                   "Z-Art",
+		"project_name":                "Z-Art",
 		"login_page_image_url":        a.config.LoginPageImageURL(),
 		"login_page_image_mode":       a.config.LoginPageImageMode(),
 		"login_page_image_zoom":       a.config.LoginPageImageZoom(),
